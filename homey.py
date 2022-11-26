86bc13bce1c6c47bb18dfcb843da8db8099a4fcd
diamonddragonbl <119129397+diamonddragonbl@users.noreply.github.com>
2022-11-26 12:50:09 -0600

    updating

--- homey.py
+++ homey.py
@@ -4,7 +4,7 @@ def start():
     password = 'Ben_is_cool'
     signing_in_or_making_account = input('are you signing in or making an account:')
     if signing_in_or_making_account == 'signing in':
-        code = input('code:')
+        code = input('password:')
         if code == password:
              mane()
     else:
@@ -15,16 +15,31 @@ def start():
             if code == password:
                 mane()
 
+def mane_again():
+    answer = input('but we do have Hangman or a calculator:')
+    if answer == 'Hangman' or answer == 'hangman':
+        import hangman1
+        hangman1.main()
+    elif answer == 'Calculator' or answer == 'calculator':
+        import calculator
+        calculator.calculator_run()
+    else:
+        print('sorry we do not have that game...')
+        mane_again()
+
 def mane():
     print(f'welcome to the home page. we have {app_number} apps')
 
     answer = input('do you want to play Hangman or use a calculator:')
-    if answer == 'Hangman':
+    if answer == 'Hangman' or answer == 'hangman':
         import hangman1
         hangman1.main()
-    else:
+    elif answer == 'Calculator' or answer == 'calculator':
         import calculator
         calculator.calculator_run()
+    else:
+        print('sorry we do not have that game...')
+        mane_again()
 
 if __name__ == "__main__":
    start()
