66d139bf4db07fd00af019830f8b2af66f56acd7
diamonddragonbl <119129397+diamonddragonbl@users.noreply.github.com>
2023-02-15 17:02:15 -0600

    updating

---
+++ game.py
@@ -0,0 +1,35 @@
+roundcount = 1
+
+aa = '  '
+ab = '  '
+ad = '  '
+
+ba = '  '
+bb = '  '
+bd = '  '
+
+da = '  '
+db = '  '
+dd = '  '
+
+def x():
+    if roundcount == 1
+        a = input('what spas')
+
+def printstatis():
+    print('' + aa + ' | ' + ab + ' | ' + ad)
+    print('-------------')
+    print(ba + ' | ' + bb + ' | ' + bd)
+    print('-------------')
+    print(da + ' | ' + db + ' | ' + dd)
+
+
+
+def run():
+    printstatis()
+    print('x turn')
+    x()
+
+
+if __name__ == "__main__":
+    run()
